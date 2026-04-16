6b6c4e2be27f8202a83b8877b846e02670d5b57f
Captainsan90 <captainsannn@gmail.com>
2026-04-16 03:27:41 +0300

    Managing Contracts

--- server/app.py
+++ server/app.py
@@ -1,10 +1,31 @@
-#!/usr/bin/env python3
+from flask import Flask
 
-from flask import Flask, request, current_app, g, make_response
-
-contracts = [{"id": 1, "contract_information": "This contract is for John and building a shed"},{"id": 2, "contract_information": "This contract is for a deck for a buisiness"},{"id": 3, "contract_information": "This contract is to confirm ownership of this car"}]
-customers = ["bob","bill","john","sarah"]
 app = Flask(__name__)
 
+# In-memory data
+contracts = {
+    "1": "This contract is for John and building a shed"
+}
+
+customers = {
+    "bob": True  # exists but returns no data (sensitive)
+}
+
+# Route: GET /contract/<id>
+@app.route('/contract/<id>', methods=['GET'])
+def get_contract(id):
+    if id in contracts:
+        return contracts[id], 200
+    return "Not found", 404
+
+
+# Route: GET /customer/<customer_name>
+@app.route('/customer/<customer_name>', methods=['GET'])
+def get_customer(customer_name):
+    if customer_name in customers:
+        return "", 204
+    return "Not found", 404
+
+
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(debug=True)
